062f32e82682f1dd8e003926d55034dced4140ee
Radhika Shetti <Radhika.shetti@nortonlifelock.com>
2023-02-20 21:27:48 -0800

    adding functionpassword Generator

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # passwordGenerator
+
+PasswordGenerator generates random password which is secure and password gets generated after gathering user inputs through a series of prompts for password preferences, 
+the rules for password generator 
+1. password length it generates min length is 8 and max length 128
+2. password will include lowercase, uppercase, numeric, and/or special character types.
+
+generated password will get displayed on the page.
+
+Technologies used:
+HTML
+CSS
+JavaScript
+
+Usage:
+Open the password generator URL in your browser.
+Generate a random password by clicking the "Generate Password" button and following the prompts.
+
+Url:
---
+++ passwordGenerator/script.js
@@ -0,0 +1,59 @@
+// Assignment code here
+function generatePassword() {
+  let numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let lowercaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let specialChar = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  let possibleChar = [];
+  let finalPwd = '';
+
+  let lengthOfPwd = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
+
+  if (lengthOfPwd < 8 || lengthOfPwd > 128) {
+    return ("please select length more than 8 or less than 128 characters")
+  } else {
+    let lowChars = prompt("do you want lowerCase chars in your password, say yes/no");
+
+    if (lowChars) {
+      possibleChar = possibleChar.concat(lowercaseChar)
+    }
+    let upperChars = prompt("do you want upper chars in your password, say yes/no");
+
+    if (upperChars) {
+      possibleChar = possibleChar.concat(uppercaseChar)
+    }
+    let numericalChar = prompt("do you want numericals in your password, say yes/no");
+    if (numericalChar) {
+      possibleChar = possibleChar.concat(numericChar)
+    }
+
+    let specialChars = prompt("do you want specialChars in your password, say yes/no");
+    if (specialChars) {
+      possibleChar = possibleChar.concat(specialChar)
+    }
+
+    if ((!lowChars) && (!upperChars) && (!numericalChar) && (!specialChars)) {
+      return ('please select at least one type of character to generate password')
+    }
+
+    for (let i = 0; i < lengthOfPwd; i++) {
+      const charToAdd = Math.floor(Math.random() * possibleChar.length);
+      finalPwd = finalPwd + possibleChar[charToAdd];
+    }
+    return finalPwd;
+  }
+}
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
